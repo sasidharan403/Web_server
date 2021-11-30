85c4b64c09beb4bba6623bc2ffd787d7d59483f6
sasisharan403 <anandsasidharan3123@gmail.com>
2021-11-30 20:51:41 +0530

    added webserver program

--- mywebsite.py
+++ mywebsite.py
@@ -1,25 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To Develop a webserver to display about top five programming languages.
-
-To develop a simple webserver to serve html pages.
-## DESIGN STEPS:
-### Step 1:
-HTML content creation
-### Step 2:
-
-Design of webserver workflow
-### Step 3:
-
-Implementation using Python code
-### Step 4:
-
-Serving the HTML pages.
-### Step 5:
-
-Testing the webserver
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -48,7 +26,4 @@ server_address = ('',8080)
 httpd = HTTPServer(server_address,myhandler)
 print("my server is running...")
 httpd.serve_forever()
-```
 
-## OUTPUT:
-## RESULT:
